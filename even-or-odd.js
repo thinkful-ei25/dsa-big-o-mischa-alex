75069c86cf585fdbeab5ca99ebfd12169e47860b
talexcrowell <talexcrowell@gmail.com>
2018-12-11 08:27:43 -0800

    even-or-odd finished

---
+++ even-or-odd.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function isEven(value){
+  let total=0;
+  if (value % 2 == 0){
+    total += 1;
+    console.log(total);
+    return true;
+  }
+  else
+    total += 1;
+    console.log(total);
+    return false;
+}
+
+// O(1)
+
+console.log(isEven(4));
+console.log(isEven(5));
