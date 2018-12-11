76e081b810f05b0d9571e1f49920bd5537041bc7
mischb <mischarberlin@gmail.com>
2018-12-11 12:06:11 -0500

    fib

---
+++ fib.js
@@ -0,0 +1,27 @@
+function generateFib(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i === 2) {
+      result.push(1);
+    }
+
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+//O(n) ==> best/worst always the same! number of itereations is param passed in to fn
+
+console.log(generateFib(9));
